6584ed239ce78222cac6781e95f2bf779e66bbc0
Sival Leão <leaosival@gmail.com>
2023-03-19 12:55:51 -0300

    initial commit

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/MainController.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/MainController.java
@@ -4,11 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
+    /*
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+     */
+    public void etc(){
+        System.out.println();
     }
 }
