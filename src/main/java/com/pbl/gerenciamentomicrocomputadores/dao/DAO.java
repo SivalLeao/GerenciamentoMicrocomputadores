13babd5a01c245ea45912aabd5cf58f5effc416e
Silvio <silviozv.oli@gmail.com>
2023-04-08 18:44:55 -0300

    docs: JavaDoc da classe DAO

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/DAO.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/DAO.java
@@ -9,6 +9,14 @@ import com.pbl.gerenciamentomicrocomputadores.dao.tecnico.TecnicoImpl;
 import com.pbl.gerenciamentomicrocomputadores.dao.peca.PecaDAO;
 import com.pbl.gerenciamentomicrocomputadores.dao.peca.PecaImpl;
 
+/** Classe usada para acessar as extensões do DAO no projeto. Os dados armazenados são divididos em quatro
+ *  entidades, que são: cliente, técnico, ordem de serviço e peça. Os atributos e métodos estão instanciados
+ *  como static, pois um objeto não precisa ser instanciado para acessar essas funções.
+ *
+ * @author Silvio Oliveira,  Sival Leão.
+ * @version 1.0.
+ */
+
 public class DAO {
 
     private static ClienteDAO clienteDAO;
@@ -19,6 +27,12 @@ public class DAO {
 
     private static PecaDAO pecaDAO;
 
+    /** Método de retorno do atributo clienteDAO, que é responsável por armazenar os dados
+     * de uma lista de clientes e acessar os seus métodos de manuseio. O objeto é inicializado
+     * como uma instância da classe ClienteImpl, caso não tenha sido anteriormente.
+     *
+     * @return ClienteDAO - objeto da classe ClienteImpl.*/
+
     public static ClienteDAO getCliente() {
 
         if (clienteDAO == null) {
@@ -27,6 +41,12 @@ public class DAO {
 
         return clienteDAO;
     }
+
+    /** Método de retorno do atributo tecnicoDAO, que é responsável por armazenar os dados
+     * de uma lista de técnicos e acessar os seus métodos de manuseio. O objeto é inicializado
+     * como uma instância da classe TecnicoImpl, caso não tenha sido anteriormente.
+     *
+     * @return TecnicoDAO - objeto da classe TecnicoImpl.*/
 
     public static TecnicoDAO getTecnico() {
 
@@ -37,6 +57,12 @@ public class DAO {
         return tecnicoDAO;
     }
 
+    /** Método de retorno do atributo ordemDeServicoDAO, que é responsável por armazenar os dados
+     * de uma lista de ordens de serviço e acessar os seus métodos de manuseio. O objeto é inicializado
+     * como uma instância da classe OrdemDeServicoImpl, caso não tenha sido anteriormente.
+     *
+     * @return OrdemDeServicoDAO - objeto da classe OrdemDeServicoImpl.*/
+
     public static OrdemDeServicoDAO getOrdemDeServico () {
 
         if (ordemDeServicoDAO == null) {
@@ -45,6 +71,12 @@ public class DAO {
 
         return ordemDeServicoDAO;
     }
+
+    /** Método de retorno do atributo pecaDAO, que é responsável por armazenar os dados
+     * de uma estrutura de armazenamento de peças e acessar os seus métodos de manuseio. O objeto
+     * é inicializado como uma instância da classe PecaImpl, caso não tenha sido anteriormente.
+     *
+     * @return PecaDAO - objeto da classe PecaImpl.*/
 
     public static PecaDAO getPeca () {
 
