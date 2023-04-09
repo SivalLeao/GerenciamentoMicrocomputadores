79f879bfd085afa7f327d27654dfd48e4a156959
Silvio <silviozv.oli@gmail.com>
2023-04-08 21:28:30 -0300

    docs: Mudanças no texto do JavaDoc do ClienteDAO.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/cliente/ClienteDAO.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/cliente/ClienteDAO.java
@@ -5,18 +5,21 @@ import com.pbl.gerenciamentomicrocomputadores.model.Cliente;
 
 /** Interface para as implementações de armazenamento dos objetos da classe Cliente. É uma
  * extensão da interface CRUD.
+ *
+ * @author Silvio Oliveira,  Sival Leão.
+ * @version 1.0.
  */
 
 public interface ClienteDAO extends CRUD<Cliente> {
 
-    /** Método de retorno de um cliente através do CPF.
+    /** Método de retorno de um cliente. Busca feita por CPF.
      *
      * @param cpf String - CPF do cliente.
      * @return Cliente - cliente encontrado após a busca por CPF.*/
 
     public Cliente findByCpf (String cpf);
 
-    /** Método para checar se um cliente está armazenado no sistema, através da busca por CPF.
+    /** Método para checar se um cliente está armazenado no sistema. Busca feita por CPF.
      *
      * @param cpf String - CPF do cliente.
      * @return boolean - resultado da busca pelo cliente. Se foi achado ou não.*/
