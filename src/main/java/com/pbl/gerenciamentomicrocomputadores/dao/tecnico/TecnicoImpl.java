d76bc7cc8e5fa064c09c078dd9c2afa2e59f457a
Silvio <silviozv.oli@gmail.com>
2023-04-08 21:56:53 -0300

    docs: JavaDoc da classe TecnicoImpl.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/tecnico/TecnicoImpl.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/tecnico/TecnicoImpl.java
@@ -5,17 +5,33 @@ import java.util.List;
 
 import com.pbl.gerenciamentomicrocomputadores.model.Tecnico;
 
+/** É responsável por armazenar todos os técnicos do sistema, e estruturar os métodos
+ * necessários para inserir, consultar, alterar ou remover. Implementa a interface TecnicoDAO.
+ *
+ * @author Silvio Oliveira,  Sival Leão.
+ * @version 1.0.
+ */
+
 public class TecnicoImpl implements TecnicoDAO {
 
     private List<Tecnico> lista;
 
     private int id;
 
+    /** Construtor que inicializa a lista de armazenamento de técnicos e o número de ID. O ID do
+     * técnico possui o 1 como número fixo na casa da unidade, modificando apenas os valores nas
+     * outras casas.*/
+
     public TecnicoImpl() {
 
         this.lista = new ArrayList<Tecnico>();
         this.id = 1111;
     }
+
+    /** Método para adicionar um técnico na lista. O ID é inserido nos dados do técnico antes de
+     * adicioná-lo na lista. O valor 10 é somado no atributo id para o próximo técnico.
+     *
+     * @param tecnico Tecnico - técnico que deve ser armazenado.*/
 
     @Override
     public void create (Tecnico tecnico) {
@@ -24,6 +40,11 @@ public class TecnicoImpl implements TecnicoDAO {
         this.id += 10;
         lista.add(tecnico);
     }
+
+    /** Método de retorno do técnico através da busca por ID.
+     *
+     * @param id int - número de ID do técnico.
+     * @return Tecnico - técnico encontrado após a busca.*/
 
     @Override
     public Tecnico findById (int id) {
@@ -39,6 +60,11 @@ public class TecnicoImpl implements TecnicoDAO {
         return null;
     }
 
+    /** Método de retorno do técnico através da busca por CPF.
+     *
+     * @param cpf String - CPF do técnico.
+     * @return Tecnico - técnico encontrado após a busca.*/
+
     @Override
     public Tecnico findByCpf (String cpf) {
 
@@ -53,6 +79,12 @@ public class TecnicoImpl implements TecnicoDAO {
         return null;
     }
 
+    /** Método para atualizar os dados de um técnico já presente no armazenamento. O ID do técnico é
+     * utilizado para encontrar seu equivalente na lista. Quando achado, o objeto antigo do técnico
+     * é substituido pelo novo.
+     *
+     * @param tecnico Tecnico - técnico que deve ser atualizado.*/
+
     @Override
     public void update (Tecnico tecnico) {
 
@@ -65,6 +97,10 @@ public class TecnicoImpl implements TecnicoDAO {
             }
         }
     }
+
+    /** Método para remover um técnico através da busca por ID.
+     *
+     * @param id int - ID do técnico que deve ser removido.*/
 
     @Override
     public void delete (int id) {
@@ -79,6 +115,10 @@ public class TecnicoImpl implements TecnicoDAO {
         }
     }
 
+    /** Método de retorno de toda a lista de técnicos armazenada no sistema.
+     *
+     * @return List<Tecnico> - lista de técnicos do sistema.*/
+
     @Override
     public List<Tecnico> findMany () {
 
@@ -91,6 +131,11 @@ public class TecnicoImpl implements TecnicoDAO {
 
         return listTecnico;
     }
+
+    /** Método para checar se um técnico está armazenado no sistema. Checagem feita através do número de ID.
+     *
+     * @param id int - número de ID do técnico.
+     * @return boolean - resultado da busca pelo técnico. Se foi achado ou não.*/
 
     @Override
     public boolean checkById(int id) {
@@ -106,6 +151,11 @@ public class TecnicoImpl implements TecnicoDAO {
         return false;
     }
 
+    /** Método para checar se um técnico está armazenado no sistema. Checagem feita através do CPF.
+     *
+     * @param cpf String - CPF do técnico.
+     * @return boolean - resultado da busca pelo técnico. Se foi achado ou não.*/
+
     @Override
     public boolean checkByCpf(String cpf) {
 
@@ -119,6 +169,9 @@ public class TecnicoImpl implements TecnicoDAO {
 
         return false;
     }
+
+    /** Método para esvaziar todo o armazenamento de técnicos. A função clear é usada para
+     * limpar a lista. A contagem de ID é resetada para o valor inicial.*/
 
     @Override
     public void deleteMany () {
