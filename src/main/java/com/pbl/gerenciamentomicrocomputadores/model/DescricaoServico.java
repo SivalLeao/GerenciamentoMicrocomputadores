df2cb2ac2168a35ed2874a799e42f25ec46e6646
Silvio <silviozv.oli@gmail.com>
2023-04-09 14:36:29 -0300

    docs: Mudanças no JavaDoc da classe DescricaoServico.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/model/DescricaoServico.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/model/DescricaoServico.java
@@ -2,55 +2,58 @@ package com.pbl.gerenciamentomicrocomputadores.model;
 
 import java.util.HashMap;
 import java.util.Map;
+
 /** Classe para objetos do tipo DescricaoServico. É responsável por armazenar as informações da descrição do
- * serviço, como o tipo e seus itens.
+ * serviço, como o tipo e os itens que devem ser utilizados, caso precise de algum.
  *
  * @author Silvio Oliveira,  Sival Leão;
  * @version 1.0
  */
+
 public class DescricaoServico {
 
     private String tipoDeServico;
 
     private Map<String, Integer> mapItens;
 
-    /** Construtor responsável por inicializa o atributo mapItens com um novo objeto da classe.
-     */
+    /** Construtor responsável por inicializar o atributo mapItens, que será responsável por
+     * armazenar os itens utilizados no serviço e a quantidade de cada um.*/
+
     public DescricaoServico() {
 
         this.mapItens = new HashMap<String, Integer>();
 
     }
 
-    /** Método para inserir o dado no atributo tipoDeServico.
+    /** Método para inserir o tipo de serviço.
      *
-     * @param tipoDeServico String - Tipo de serviço
-     */
+     * @param tipoDeServico String - Tipo de serviço*/
+
     public void setTipoDeServico(String tipoDeServico) {
         this.tipoDeServico = tipoDeServico;
     }
 
-    /** Método que retorna tipoDeServico.
+    /** Método que retorna o tipo de serviço.
      *
-     * @return String - Tipo de serviço
-     */
+     * @return String - Tipo de serviço*/
+
     public String getTipoDeServico() {
         return this.tipoDeServico;
     }
 
-    /** Método usado para adicionar um novo item ao mapa de itens do serviço.
+    /** Método usado para adicionar uma nova peça na estrutura de armazenamento com as peças utilizadas no serviço.
      *
      * @param nomePeca String - Nome da peça
-     * @param quantidade int _ Quantidade da peça
-     */
+     * @param quantidade int - Quantidade da peça*/
+    
     public void setMapItens (String nomePeca, int quantidade) {
         this.mapItens.put(nomePeca, quantidade);
     }
 
-    /** Método que retorna um mapa de itens do serviço.
+    /** Método que retorna um map com os itens utilizados no serviço e a quantidade de cada um.
      *
-     * @return Map - Mapa de itens do serviço.
-     */
+     * @return Map - Mapa de itens do serviço.*/
+
     public Map<String, Integer> getMapItens () {
 
         Map<String, Integer> mapItens = new HashMap<String, Integer>();
