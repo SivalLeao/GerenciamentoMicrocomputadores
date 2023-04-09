29396a9f269bdee10af6c0eaa62f190d5a48c970
Silvio <silviozv.oli@gmail.com>
2023-04-09 14:01:11 -0300

    test: Exclusão de arquivos sem utilidade.

--- src/test/java/com/pbl/gerenciamentomicrocomputadores/model/GMTestSuit.java
+++
@@ -1,6 +0,0 @@
-package com.pbl.gerenciamentomicrocomputadores.model;
-
-@SelectPackages({"com.pbl.gerenciamentomicrocomputadores.dao", "com.pbl.gerenciamentomicrocomputadores.model"})
-@Suite
-public class GMTestSuit {
-}
--- src/test/java/com/pbl/gerenciamentomicrocomputadores/model/Suite.java
+++
@@ -1,4 +0,0 @@
-package com.pbl.gerenciamentomicrocomputadores.model;
-
-public @interface Suite {
-}
