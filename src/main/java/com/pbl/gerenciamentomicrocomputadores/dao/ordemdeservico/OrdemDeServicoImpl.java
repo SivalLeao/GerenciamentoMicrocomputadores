eb65fc0b681b602c888a253549bb34d0562a9e87
Sival Leão <leaosival@gmail.com>
2023-04-09 16:05:33 -0300

    docs: JavaDoc da classe OrdemDeServicoImpl.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/ordemdeservico/OrdemDeServicoImpl.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/ordemdeservico/OrdemDeServicoImpl.java
@@ -7,19 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/** É responsável por armazenar todas as ordem de serviço do sistema, e estruturar os métodos
+ * necessários para inserir, consultar, alterar ou remover. Implementa a interface OrdemDeServico.
+ *
+ * @author Silvio Oliveira,  Sival Leão.
+ * @version 1.0.
+ */
 public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
 
     private List<OrdemDeServico> lista;
 
     private int id;
 
+    /** Construtor que inicializa a lista de armazenamento da ordem de serviço e o número de ID. O ID da
+     * ordem possui o 3 como número fixo na casa da unidade, modificando apenas os valores nas
+     * outras casas.
+     * */
     public OrdemDeServicoImpl () {
 
         this.lista = new ArrayList<OrdemDeServico>();
         this.id = 1113;
     }
 
-
+    /** Método para adicionar uma ordem de serviço na lista. O ID é inserido nos dados da ordem antes de
+     *  adicioná-lo na lista. O valor 10 é somado no atributo id para a próxima ordem de serviço.
+     *
+     * @param ordemDeServico OrdemDeServico - ordem de serviço que deve ser armazenado.
+     */
     @Override
     public void create (OrdemDeServico ordemDeServico) {
 
@@ -29,6 +43,11 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         this.lista.add(ordemDeServico);
     }
 
+    /** Método de retorno da ordem de serviço através da busca por ID.
+     *
+     * @param idOrdem int - número de ID.
+     * @return OrdemDeServico - ordem encontrada após a busca
+     */
     @Override
     public OrdemDeServico findById (int idOrdem) {
 
@@ -43,6 +62,12 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         return null;
     }
 
+    /** Método para atualizar os dados de uma ordem de serviço já presente no armazenamento. O ID da ordem é
+     * utilizado para encontrar seu equivalente na lista. Quando achado, o objeto antigo da ordem de serviço
+     * é substituido pelo novo.
+     *
+     * @param ordemDeServico OrdemDeServico - ordem de serviço que deve ser atualizado.
+     */
     @Override
     public void update (OrdemDeServico ordemDeServico) {
 
@@ -56,6 +81,13 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         }
     }
 
+    /** Método para atualizar os dados dos status de uma ordem de serviço já presente no armazenamento. O ID da ordem é
+     *  utilizado para encontrar seu equivalente na lista. Quando achado, o valor do status é atualizado.
+     *
+     * @param idOrdem int - número de ID.
+     * @param status String - status do serviço
+     * @return Map<String, Integer> - mapa contendo itens de peça
+     */
     public Map<String, Integer> updateStatus (int idOrdem, String status) {
 
         for (int i = 0; i < this.lista.size(); i++) {
@@ -81,6 +113,10 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         return null;
     }
 
+    /** Método para remover uma ordem de serviço através da busca por ID.
+     *
+     * @param idOrdem int - ID do objeto que deve ser removido.
+     */
     @Override
     public void delete (int idOrdem) {
 
@@ -94,6 +130,10 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         }
     }
 
+    /** Método de retorno de toda a lista de ordem de serviço armazenada no sistema.
+     *
+     * @return List<OrdemDeServico> - lista de ordem de serviço do sistema
+     */
     @Override
     public List<OrdemDeServico> findMany () {
 
@@ -107,6 +147,12 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         return listOrdemDeServico;
     }
 
+    /** Método que retorna uma lista de ordens de serviço atribuídas a um técnico específico
+     * armazenadas no sistema.
+     *
+     * @param idTecnico int - id do técnico
+     * @return List<OrdemDeServico> - lista de ordens de serviço atribuídas a um técnico específico
+     */
     @Override
     public List<OrdemDeServico> findByIdTecnico (int idTecnico) {
 
@@ -123,6 +169,12 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         return listOrdemDeServico;
     }
 
+    /** Método que retorna uma lista de ordens de serviço em aberto atribuídas a um técnico específico
+     * armazenadas no sistema.
+     *
+     * @param idTecnico int - id do técnico
+     * @return lista de ordens de serviço em aberto atribuídas a um técnico específico
+     */
     @Override
     public List<OrdemDeServico> openListTecnico (int idTecnico) {
 
@@ -142,6 +194,11 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         return listOrdemDeServico;
     }
 
+    /** Método para checar se uma ordem de serviço está armazenado no sistema. Checagem feita através do número de ID.
+     *
+     * @param idOrdem int - número do ID da ordem de serviço.
+     * @return boolean - resultado da busca pelo cliente. Se foi achado ou não.
+     */
     @Override
     public boolean checkById (int idOrdem) {
 
@@ -156,6 +213,12 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         return false;
     }
 
+    /** Método para checar os status de uma ordem de serviço está armazenado no sistema.
+     * Checagem feita através do número de ID do técnico.
+     *
+     * @param idTecnico int - id do técnico
+     * @return boolean - retorna verdadeiro se a ordem de serviço estiver em andamento
+     */
     @Override
     public boolean checkStatus (int idTecnico) {
 
@@ -170,11 +233,13 @@ public class OrdemDeServicoImpl implements OrdemDeServicoDAO {
         return false;
     }
 
+    /** Método para esvaziar o armazenamento de ordem de serviço. A função clear é usada para
+     * limpar a lista. A contagem de ID é resetada para o valor inicial
+     */
     @Override
     public void deleteMany () {
 
         this.lista.clear();
         this.id = 1113;
     }
-
 }
