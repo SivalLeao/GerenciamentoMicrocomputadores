4ac89b609abdf5d915be77f41ec75608faadf9ae
Silvio <silviozv.oli@gmail.com>
2023-04-10 12:35:22 -0300

    feat: Teste da impressão da descrição do serviço.

---
+++ src/test/java/com/pbl/gerenciamentomicrocomputadores/model/DescricaoServicoTest.java
@@ -0,0 +1,36 @@
+package com.pbl.gerenciamentomicrocomputadores.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DescricaoServicoTest {
+
+    @Test
+    void imprimirDescricaoServico () {
+
+        OrdemDeServico ordem0 = new OrdemDeServico( 1111, 1112);
+
+        ordem0.getDescricaoServico().setTipoDeServico("Montagem/Instalação");
+        ordem0.getDescricaoServico().setMapItens("ram", 3);
+        ordem0.getDescricaoServico().setMapItens("hd", 1);
+
+        assertEquals("""
+                Descrição: Montagem/Instalação de peças
+                Peças:
+                
+                Peça 1 - nome: hd ; quantidade: 1
+                Peça 2 - nome: ram ; quantidade: 3
+                """, ordem0.getDescricaoServico().imprimirDescricaoServico());
+
+        OrdemDeServico ordem1 = new OrdemDeServico( 1121, 1122);
+
+        ordem1.getDescricaoServico().setTipoDeServico("Programas");
+
+        assertEquals("""
+                Descrição: Formatação/Instalação de programas
+                """, ordem1.getDescricaoServico().imprimirDescricaoServico());
+
+    }
+
+}
