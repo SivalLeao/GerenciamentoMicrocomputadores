4a8904352dab42945dce0aa991c09722d0aa7e9e
Sival Leão <leaosival@gmail.com>
2023-04-21 19:51:32 -0300

    feat: Criação dos métodos da classe.
    Foram criados dois métodos responsáveis por salvar e resgatar os dados do sistema.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/utils/ArmazenamentoDeDados.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/utils/ArmazenamentoDeDados.java
@@ -1,13 +1,39 @@
 package com.pbl.gerenciamentomicrocomputadores.utils;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArmazenamentoDeDados {
+    public static <T> void guardarDados (List<T> lista, String nomeArquivo) throws IOException {
+        File diretorio = new File("dados salvo");
+        diretorio.mkdir();
 
-    public static void guardarDados() {
+        File arquivo = new File(diretorio, nomeArquivo);
+        FileOutputStream fos = new FileOutputStream(arquivo);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
 
+        oos.writeObject(lista);
+
+        fos.close();
+        oos.close();
     }
+    public static <T> List resgatarDados(String nomeArquivo) throws IOException, ClassNotFoundException {
+        File direttorio = new File("dados salvo");
+        File arquivo = new File(direttorio, nomeArquivo);
 
-    public static void resgatarDados() {
+        if (!arquivo.exists()){
+            return new ArrayList<T>();
+        }
+        FileInputStream fis = new FileInputStream(arquivo);
+        ObjectInputStream ois = new ObjectInputStream(fis);
 
+        List<T> lista = (List<T>) ois.readObject();
+
+        fis.close();
+        ois.close();
+
+        return lista;
     }
 
 }
