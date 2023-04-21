5cae857769336ec21137a0815cb1cf6ae39fdd4e
Silvio <silviozv.oli@gmail.com>
2023-04-21 14:36:57 -0300

    feat: Criação de classe.
     Criação da classe de manipulação dos arquivos de armazenamento.

---
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/utils/ArmazenamentoDeDados.java
@@ -0,0 +1,13 @@
+package com.pbl.gerenciamentomicrocomputadores.utils;
+
+public class ArmazenamentoDeDados {
+
+    public static void guardarDados() {
+
+    }
+
+    public static void resgatarDados() {
+
+    }
+
+}
