459c64fabe2a99686d0c181de3b1fe8cb9908126
Silvio <silviozv.oli@gmail.com>
2023-04-23 16:58:40 -0300

    feat: Mudanças na classe de ArmazenamentoDeDados.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/utils/ArmazenamentoDeDados.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/utils/ArmazenamentoDeDados.java
@@ -5,42 +5,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArmazenamentoDeDados {
-    public static <T> void guardarDados (List<T> lista, String nomeArquivo, String nomePasta) throws IOException {
-        File diretorio = new File("dados salvo");
-        diretorio.mkdir();
 
-        String localDiretorio = diretorio.getAbsolutePath();
+    public static <T> void guardarDados (List<T> lista, String nomeArquivo, String nomePasta) {
 
-        File pasta = new File(localDiretorio +"/"+ nomePasta);
-        pasta.mkdir();
+        try {
 
-        File arquivo = new File(pasta, nomeArquivo);
-        FileOutputStream fos = new FileOutputStream(arquivo);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
+            File diretorio = new File("dados salvos");
+            diretorio.mkdir();
+            String localDiretorio = diretorio.getAbsolutePath();
+            File pasta = new File(localDiretorio +"/"+ nomePasta);
+            pasta.mkdir();
 
-        oos.writeObject(lista);
+            File arquivo = new File(pasta, nomeArquivo);
+            FileOutputStream fos = new FileOutputStream(arquivo);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-        fos.close();
-        oos.close();
-    }
-    public static <T> List resgatarDados(String nomeArquivo, String nomePasta) throws IOException, ClassNotFoundException {
-        File diretorio = new File("dados salvo");
-        String localDiretorio = diretorio.getAbsolutePath();
-        File pasta = new File(localDiretorio +"/"+ nomePasta);
-        File arquivo = new File(pasta, nomeArquivo);
+            oos.writeObject(lista);
 
-        if (!arquivo.exists()){
-            return new ArrayList<T>();
+            fos.close();
+            oos.close();
         }
-        FileInputStream fis = new FileInputStream(arquivo);
-        ObjectInputStream ois = new ObjectInputStream(fis);
+        catch ( java.io.FileNotFoundException e) {
 
-        List<T> lista = (List<T>) ois.readObject();
+            throw new RuntimeException("Arquivo não encontrado");
+        }
+        catch ( java.io.IOException e) {
 
-        fis.close();
-        ois.close();
+            throw new RuntimeException("Erro ao acessar dados");
+        }
 
-        return lista;
+    }
+
+    public static <T> ArrayList<T> resgatarDados(String nomeArquivo, String nomePasta) {
+
+        try {
+
+            File diretorio = new File("dados salvos");
+            String localDiretorio = diretorio.getAbsolutePath();
+            File pasta = new File(localDiretorio + "/" + nomePasta);
+            File arquivo = new File(pasta, nomeArquivo);
+
+            if (!arquivo.exists()) {
+
+                return new ArrayList<T>();
+            }
+
+            FileInputStream fis = new FileInputStream(arquivo);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            List<T> lista = (ArrayList<T>) ois.readObject();
+
+            fis.close();
+            ois.close();
+
+            return (ArrayList<T>) lista;
+        }
+        catch ( java.io.IOException e) {
+
+            throw new RuntimeException("Arquivo não encontrado");
+        }
+        catch ( java.lang.ClassNotFoundException e) {
+
+            throw new RuntimeException("Classe não encontrada");
+        }
+
     }
 
 }
