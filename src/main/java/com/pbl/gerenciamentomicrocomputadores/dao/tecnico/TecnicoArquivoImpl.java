90404a1d1fe854a5d978eba773d7eff0a5fb163d
Sival Leão <leaosival@gmail.com>
2023-04-29 22:20:26 -0300

    docs: JavaDoc da classe TecnicoArquivoImpl.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/tecnico/TecnicoArquivoImpl.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/tecnico/TecnicoArquivoImpl.java
@@ -6,18 +6,33 @@ import com.pbl.gerenciamentomicrocomputadores.utils.ArmazenamentoDeDados;
 import java.util.ArrayList;
 import java.util.List;
 
+/** É responsável por armazenar e todos os técnicos do sistema em um arquivo binário com extensão ".dat", e estruturar
+ * os métodos necessários para inserir, consultar, alterar ou remover. Implementa a interface TecnicoDAO.
+ *
+ * @author Silvio Oliveira,  Sival Leão.
+ * @version 3.0.
+ */
+
 public class TecnicoArquivoImpl implements TecnicoDAO {
 
     private List<Tecnico> lista;
     private String nomeArquivo = "tecnico.dat";
     private String nomePasta = "Tecnico";
 
+    /** Construtor responsável por resgatar uma lista contendo os dados dos técnicos cadastrados no sistema
+     * que foram que foram previamente salvos em um arquivo binário. Caso não haja nenhum cadastro, o construtor
+     * retorna uma lista vazia.*/
     public TecnicoArquivoImpl() {
 
         this.lista = ArmazenamentoDeDados.resgatarDados(nomeArquivo,nomePasta);
 
     }
 
+    /** Método para adicionar um técnico na lista e em seguida é salvo em um arquivo binário. O ID é inserido nos
+     * dados do técnico antes de adicioná-lo no arquivo. O ID do técnico inicialmente tem o valor 1111, o valor
+     * 10 é somado  no atributo id para o próximo técnico.
+     *
+     * @param tecnico Tecnico - técnico que deve ser armazenado.*/
     @Override
     public void criar(Tecnico tecnico) {
 
@@ -40,6 +55,10 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
 
     }
 
+    /** Método de retorno do técnico através da busca por ID.
+     *
+     * @param id int - número de ID do técnico.
+     * @return Tecnico - técnico encontrado após a busca.*/
     @Override
     public Tecnico encontrarPorId(int id) {
 
@@ -54,6 +73,10 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         return null;
     }
 
+    /** Método de retorno do técnico através da busca por CPF.
+     *
+     * @param cpf String - CPF do técnico.
+     * @return Tecnico - técnico encontrado após a busca.*/
     @Override
     public Tecnico encontrarPorCpf(String cpf) {
 
@@ -68,6 +91,12 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         return null;
     }
 
+    /** Método para atualizar os dados de um técnico já presente no armazenamento. O ID do técnico é
+     * utilizado para encontrar seu equivalente na lista. Quando achado, o objeto antigo do técnico
+     * é substituido pelo novo.
+     *
+     * @param tecnico Tecnico - técnico que deve ser atualizado.
+     */
     @Override
     public void atualizar(Tecnico tecnico) {
 
@@ -84,6 +113,9 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         }
     }
 
+    /** Método para remover um técnico através da busca por ID.
+     *
+     * @param id int - ID do técnico que deve ser removido.*/
     @Override
     public void remover(int id) {
 
@@ -100,6 +132,9 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         }
     }
 
+    /** Método de retorno de toda a lista de técnicos armazenada no sistema.
+     *
+     * @return List - lista de técnicos do sistema.*/
     @Override
     public List<Tecnico> encontrarTodos() {
 
@@ -113,6 +148,10 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         return listTecnico;
     }
 
+    /** Método para checar se um técnico está armazenado no sistema. Checagem feita através do número de ID.
+     *
+     * @param id int - número de ID do técnico.
+     * @return boolean - resultado da busca pelo técnico. Se foi achado ou não.*/
     @Override
     public boolean checarPorId(int id) {
 
@@ -127,6 +166,10 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         return false;
     }
 
+    /** Método para checar se um técnico está armazenado no sistema. Checagem feita através do CPF.
+     *
+     * @param cpf String - CPF do técnico.
+     * @return boolean - resultado da busca pelo técnico. Se foi achado ou não.*/
     @Override
     public boolean checarPorCpf(String cpf) {
 
@@ -141,6 +184,8 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         return false;
     }
 
+    /** Método para esvaziar todo o armazenamento de técnicos. A função clear é usada para
+     * limpar a lista.*/
     @Override
     public void removerTodos() {
 
@@ -148,6 +193,7 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         ArmazenamentoDeDados.guardarDados(lista, nomeArquivo, nomePasta);
     }
 
+    /** Método responsável por mudar o endereço do arquivo quando for necessário realizar testes de unidade.*/
     @Override
     public void diretorioTest() {
 
@@ -155,6 +201,7 @@ public class TecnicoArquivoImpl implements TecnicoDAO {
         this.nomePasta = "Teste Tecnico";
     }
 
+    /** Método responsável por mudar para o endereço padrão do arquivo após a conclusão dos testes de unidade.*/
     @Override
     public void diretorioPadrao() {
 
