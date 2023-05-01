4616eb4933778f824536b93a0bf1355012b9c358
Silvio <silviozv.oli@gmail.com>
2023-04-30 22:28:31 -0300

    docs: JavaDoc de OrdemDeServicoArquivoImpl.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/ordemdeservico/OrdemDeServicoArquivoImpl.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/dao/ordemdeservico/OrdemDeServicoArquivoImpl.java
@@ -8,16 +8,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/** É responsável por armazenar todas as ordens de serviço do sistema em um arquivo binário
+ * com extensão ".dat", e estruturar os métodos necessários para inserir, consultar, alterar
+ * ou remover. Implementa a interface OrdemDeServicoDAO.
+ *
+ * @author Silvio Oliveira,  Sival Leão.
+ * @version 3.0.
+ */
+
 public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
 
     private List<OrdemDeServico> lista;
     private String nomeArquivo = "ordemdeservico.dat";
     private String nomePasta = "Ordem De Servico";
 
+    /** Construtor responsável por resgatar a lista contendo os dados das ordens de serviço
+     * cadastradas no sistema, que foram previamente salvas em um arquivo binário.*/
+
     public OrdemDeServicoArquivoImpl () {
 
         this.lista =  ArmazenamentoDeDados.resgatarDados(nomeArquivo,nomePasta);
     }
+
+    /** Método para adicionar uma ordem de serviço na lista e em seguida salvar em um arquivo binário.
+     * O ID da última ordem de serviço cadastrada é usado como base para a próxima, somando o valor 10 ao ID
+     * encontrado e inserindo nos dados da ordem de serviço que deve ser adicionada. Se a lista de ordens de
+     * serviço estiver vazia, o ID inicial é 1113. O status da ordem é colocado como "Em espera".
+     *
+     * @param ordemDeServico OrdemDeServico - ordem de serviço que deve ser armazenada.*/
 
     @Override
     public void criar(OrdemDeServico ordemDeServico) {
@@ -40,6 +58,11 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
         ArmazenamentoDeDados.guardarDados(lista, nomeArquivo,nomePasta);
     }
 
+    /** Método de retorno da ordem de serviço através da busca por ID.
+     *
+     * @param idOrdem int - número de ID da ordem de Serviço
+     * @return OrdemDeServico - ordem de serviço encontrada após a busca*/
+
     @Override
     public OrdemDeServico encontrarPorId(int idOrdem) {
 
@@ -53,6 +76,12 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
 
         return null;
     }
+
+    /** Método para atualizar os dados de uma ordem de serviço já presente no armazenamento. O ID da ordem
+     * é utilizado para encontrar seu equivalente na lista. Quando achado, o objeto antigo da ordem é
+     * substituido pelo novo, e o arquivo binário é reescrito com os novos dados.
+     *
+     * @param ordemDeServico OrdemDeServico - ordem de serviço que deve ser atualizada.*/
 
     @Override
     public void atualizar(OrdemDeServico ordemDeServico) {
@@ -69,6 +98,17 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
             }
         }
     }
+
+    /** Método para atualizar os status de uma ordem de serviço. O ID da ordem é utilizado para encontrar
+     * seu equivalente na lista. Quando achado, o valor do status é atualizado. Se o novo status for de
+     * "Finalizado", a data final de realização do serviço é inserida no objeto da ordem. Após as mudanças,
+     * o arquivo binário é reescrito com os novos dados. Caso o novo status seja de "Cancelado", a lista
+     * de peças utilizadas é retornada para ser devolvida ao estoque.
+     *
+     * @param idOrdem int - número de ID da ordem de serviço
+     * @param status String - novo status da ordem de serviço
+     * @return Map - estrutura HashMap contendo as peças utilizadas no serviço que devem
+     * ser devolvidas ao estoque, caso a ordem de serviço seja cancelada*/
 
     public Map<String, Integer> atualizarStatus(int idOrdem, String status) {
 
@@ -100,6 +140,11 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
         return null;
     }
 
+    /** Método para remover uma ordem de serviço através da busca por ID. Após a remoção, o arquivo
+     * binário de armazenamento é reescrito com os novos dados.
+     *
+     * @param idOrdem int - ID da ordem de servico que deve ser removida.*/
+
     @Override
     public void remover(int idOrdem) {
 
@@ -116,6 +161,10 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
         }
     }
 
+    /** Método de retorno de todas as ordens de serviço armazenadas no sistema.
+     *
+     * @return List - lista de ordens de serviço do sistema*/
+
     @Override
     public List<OrdemDeServico> encontrarTodos() {
 
@@ -128,6 +177,12 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
 
         return listOrdemDeServico;
     }
+
+    /** Método de retorno da lista de ordens de serviço que devem ser realizadas por determinado técnico.
+     * A busca pelas ordens é feita através do ID do técnico.
+     *
+     * @param idTecnico int - ID do técnico
+     * @return List - lista de ordens de serviço atribuídas a um técnico específico*/
 
     @Override
     public List<OrdemDeServico> encontrarPorIdTecnico(int idTecnico) {
@@ -144,6 +199,13 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
 
         return listOrdemDeServico;
     }
+
+    /** Método de retorno da lista de ordens de serviço em aberto de um determinado técnico. A busca pelas
+     * ordens é feita através do ID do técnico. As ordens em aberto são indicadas se estiverem com o
+     * status "Em andamento" ou "Em espera".
+     *
+     * @param idTecnico int - ID do técnico
+     * @return List - lista de ordens de serviço em aberto atribuídas a um técnico específico*/
 
     @Override
     public List<OrdemDeServico> listaEmAbertoTecnico(int idTecnico) {
@@ -164,6 +226,12 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
         return listOrdemDeServico;
     }
 
+    /** Método para checar se uma ordem de serviço está armazenada no sistema. Checagem feita através
+     * do número de ID da ordem.
+     *
+     * @param idOrdem int - número de ID da ordem de serviço.
+     * @return boolean - resultado da busca pela ordem de serviço. Se foi achada ou não.*/
+
     @Override
     public boolean checarPorId(int idOrdem) {
 
@@ -177,6 +245,13 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
 
         return false;
     }
+
+    /** Método para checar se existe alguma ordem de serviço, atribuída a um técnico específico,
+     * com status "Em andamento". Checagem feita através do ID do técnico.
+     *
+     * @param idTecnico int - ID do técnico
+     * @return boolean - resultado da busca por uma ordem de serviço, com status "Em andamento",
+     * atribuída a um técnico. Se foi achada ou não*/
 
     @Override
     public boolean checarStatusEmAndamento(int idTecnico) {
@@ -192,6 +267,9 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
         return false;
     }
 
+    /** Método para esvaziar toda a lista de ordens de serviço, usando a função clear. O arquivo binário de
+     * armazenamento é reescrito com a lista vazia.*/
+
     @Override
     public void removerTodos() {
 
@@ -200,11 +278,17 @@ public class OrdemDeServicoArquivoImpl implements OrdemDeServicoDAO {
         ArmazenamentoDeDados.guardarDados(lista, nomeArquivo,nomePasta);
     }
 
+    /** Método responsável por mudar o endereço do arquivo e da pasta de armazenamneto da lista
+     *  quando for necessário realizar testes de unidade.*/
+
     public void diretorioTest() {
 
         this.nomeArquivo = "ordemdeservicotest.dat";
         this.nomePasta = "Teste Ordem De Servico";
     }
+
+    /** Método responsável por mudar o arquivo e a pasta de armazenamento para os endereços padrões
+     * após a conclusão dos testes de unidade.*/
 
     @Override
     public void diretorioPadrao() {
