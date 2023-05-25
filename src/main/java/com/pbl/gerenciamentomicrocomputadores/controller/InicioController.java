a74d5cf9e8926e3e795bef27a10c0dd6f41a20f3
Silvio Azevedo <109181824+Silviozv@users.noreply.github.com>
2023-05-25 16:33:04 -0300

    feat: Canto inicial da tela de início.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/InicioController.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/InicioController.java
@@ -1,11 +1,14 @@
 package com.pbl.gerenciamentomicrocomputadores.controller;
 
 import com.pbl.gerenciamentomicrocomputadores.MainApplication;
+import com.pbl.gerenciamentomicrocomputadores.dao.DAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -35,6 +38,24 @@ public class InicioController {
     private Pane barraRetraida;
 
     @FXML
+    private Pane paneCantoInicio;
+
+    @FXML
+    private Pane paneTecnicoLogado;
+
+    @FXML
+    private Button loginBotao;
+
+    @FXML
+    private Button cadastrarBotao;
+
+    @FXML
+    private Label nomeTecnico;
+
+    @FXML
+    private Label idTecnico;
+
+    @FXML
     void initialize () {
 
         barraInteira.setVisible(false);
@@ -46,6 +67,9 @@ public class InicioController {
         barraRetraida.setOnMouseEntered(event -> {barraInteira.setVisible(true);
             containerBarraRetraida.setVisible(false);
         });
+
+        paneCantoInicio.setVisible(true);
+        paneTecnicoLogado.setVisible(false);
 
     }
 
@@ -121,5 +145,27 @@ public class InicioController {
 
     }
 
+
+    @FXML
+    void abaLogin (ActionEvent event) {
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("LoginView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 400, 200);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("/com/pbl/gerenciamentomicrocomputadores/Icones/Icone.png")));
+            stage.setScene(scene);
+
+            LoginController.setStage(stage);
+
+            stage.show();
+        }
+        catch (java.io.IOException e) {
+
+        }
+
+    }
 
 }
