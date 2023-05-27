584bfe6d8752f4b842ebc07c307a3d4da2f224da
Sival Leão <leaosival@gmail.com>
2023-05-27 10:46:57 -0300

    feat: Criação de uma nova aba.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/LoginController.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/LoginController.java
@@ -1,18 +1,26 @@
 package com.pbl.gerenciamentomicrocomputadores.controller;
 
+import com.pbl.gerenciamentomicrocomputadores.MainApplication;
 import com.pbl.gerenciamentomicrocomputadores.dao.DAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class LoginController {
 
     private static Stage stage;
 
+
+    @FXML
+    private Button Cadastrar;
     @FXML
     private TextField cpfTecnico;
 
@@ -45,12 +53,37 @@ public class LoginController {
 
     @FXML
     void fecharAbaLogin (ActionEvent event) {
-
+        Stage stage = (Stage) voltarBotao.getScene().getWindow();
         stage.close();
+    }
+    @FXML
+    void novoCadastro(ActionEvent event) {
+        Stage stage = (Stage) voltarBotao.getScene().getWindow();
+        stage.close();
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("CadastrarTecnicoView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage = new Stage();
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setResizable(false);
+            stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("/com/pbl/gerenciamentomicrocomputadores/Icones/Icone.png")));
+            stage.setScene(scene);
+
+            LoginController.setStage(stage);
+
+            stage.show();
+        }
+        catch (java.io.IOException e) {
+
+        }
+
     }
 
     public static Stage getStage () { return stage; }
 
     public static void setStage (Stage stage) { LoginController.stage = stage; }
+
+
 
 }
