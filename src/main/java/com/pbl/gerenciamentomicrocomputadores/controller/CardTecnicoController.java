ced058605c61a74157dfe80c5294a9067b5493fe
Silvio <silviozv.oli@gmail.com>
2023-06-03 14:50:00 -0300

    feat: Criando card de exibição do técnico.

---
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/CardTecnicoController.java
@@ -0,0 +1,22 @@
+package com.pbl.gerenciamentomicrocomputadores.controller;
+
+import com.pbl.gerenciamentomicrocomputadores.model.Tecnico;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CardTecnicoController {
+
+    @FXML
+    private Label idTecnico;
+
+    @FXML
+    private Label nomeTecnico;
+
+    public void setInfo (Tecnico tecnico) {
+
+        nomeTecnico.setText(tecnico.getNome());
+        idTecnico.setText(Integer.toString(tecnico.getId()));
+
+    }
+
+}
