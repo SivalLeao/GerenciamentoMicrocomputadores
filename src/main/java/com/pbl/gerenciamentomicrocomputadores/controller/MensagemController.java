ef5dc1b7a58491ea7e18c845858900b005870b3d
Silvio <silviozv.oli@gmail.com>
2023-06-08 22:03:51 -0300

    feat: Colocando botão de confirmar na mensagem.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/MensagemController.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/MensagemController.java
@@ -1,12 +1,23 @@
 package com.pbl.gerenciamentomicrocomputadores.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class MensagemController {
 
-    @FXML
-    private Label mensagem;
+    @FXML private Label mensagem;
+
+    @FXML private Button confirmarBotao;
+
+    @FXML void confirmarAcao(ActionEvent event) {
+
+        Stage stage = (Stage) confirmarBotao.getScene().getWindow();
+        stage.close();
+
+    }
 
     public void setMensagem (String mensagem) { this.mensagem.setText(mensagem);}
 
