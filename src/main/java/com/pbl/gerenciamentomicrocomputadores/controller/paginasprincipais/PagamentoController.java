494fce942fd4e67e20713584e2429082ee6b7b75
Silvio <silviozv.oli@gmail.com>
2023-06-15 21:30:16 -0300

    feat: Criando página de pagamento.

---
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/paginasprincipais/PagamentoController.java
@@ -0,0 +1,261 @@
+package com.pbl.gerenciamentomicrocomputadores.controller.paginasprincipais;
+
+import com.pbl.gerenciamentomicrocomputadores.MainApplication;
+import com.pbl.gerenciamentomicrocomputadores.controller.MainController;
+import com.pbl.gerenciamentomicrocomputadores.dao.DAO;
+import com.pbl.gerenciamentomicrocomputadores.model.Tecnico;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class PagamentoController {
+
+    @FXML private Button inicioBotao;
+    @FXML private Button clienteBotao;
+    @FXML private Button tecnicoBotao;
+    @FXML private Button estoqueBotao;
+    @FXML private Button ordemBotao;
+
+    @FXML private Pane paneCantoInicio;
+    @FXML private Button cadastrarBotao;
+    @FXML private Button loginBotao;
+
+    @FXML private Pane paneTecnicoLogado;
+    @FXML private Label nomeTecnico;
+    @FXML private Label idTecnico;
+    @FXML private Button deslogarBotao;
+
+    @FXML
+    void initialize () {
+
+        paneCantoInicio.setVisible(true);
+        paneTecnicoLogado.setVisible(false);
+
+    }
+
+    @FXML
+    void abaInicio(ActionEvent event) {
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("InicioView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 650);
+            Stage stage = MainController.getStageInicio();
+            stage.setScene(scene);
+
+            MainController.setStageInicio(stage);
+            MainController.setFXMLLoaderPrincipal(fxmlLoader);
+
+            if (!(idTecnico.getText().equals(""))) {
+
+                InicioController inicioController = fxmlLoader.getController();
+                inicioController.fazendoMudancaLogin(DAO.getTecnico().encontrarPorId(
+                        Integer.parseInt(idTecnico.getText())));
+            }
+
+            stage.show();
+        }
+        catch (java.io.IOException e) {
+
+        }
+    }
+
+    @FXML
+    void abaCliente (ActionEvent event) {
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("ClienteView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 650);
+            Stage stage = MainController.getStageInicio();
+            stage.setScene(scene);
+
+            MainController.setStageInicio(stage);
+            MainController.setFXMLLoaderPrincipal(fxmlLoader);
+
+            if (!(idTecnico.getText().equals(""))) {
+
+                ClienteController clienteController = fxmlLoader.getController();
+                clienteController.fazendoMudancaLogin(DAO.getTecnico().encontrarPorId(
+                        Integer.parseInt(idTecnico.getText())));
+            }
+
+            stage.show();
+        }
+        catch (java.io.IOException e) {
+
+        }
+
+    }
+
+    @FXML
+    void abaTecnico(ActionEvent event) {
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("TecnicoView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 650);
+            Stage stage = MainController.getStageInicio();
+            stage.setScene(scene);
+
+            MainController.setStageInicio(stage);
+            MainController.setFXMLLoaderPrincipal(fxmlLoader);
+
+            if (!(idTecnico.getText().equals(""))) {
+
+                TecnicoController tecnicoController = fxmlLoader.getController();
+                tecnicoController.fazendoMudancaLogin(DAO.getTecnico().encontrarPorId(
+                        Integer.parseInt(idTecnico.getText())));
+            }
+
+            stage.show();
+        }
+        catch (java.io.IOException e) {
+
+        }
+
+    }
+
+    @FXML
+    void abaOrdem (ActionEvent event) {
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("OrdemView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 650);
+            Stage stage = MainController.getStageInicio();
+            stage.setScene(scene);
+
+            MainController.setStageInicio(stage);
+            MainController.setFXMLLoaderPrincipal(fxmlLoader);
+
+            if (!(idTecnico.getText().equals(""))) {
+
+                OrdemController ordemController = fxmlLoader.getController();
+                ordemController.fazendoMudancaLogin(DAO.getTecnico().encontrarPorId(
+                        Integer.parseInt(idTecnico.getText())));
+            }
+
+            stage.show();
+        }
+        catch (java.io.IOException e) {
+
+        }
+
+    }
+
+    @FXML
+    void abaEstoque (ActionEvent event) {
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("EstoqueView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 650);
+            Stage stage = MainController.getStageInicio();
+            stage.setScene(scene);
+
+            MainController.setStageInicio(stage);
+            MainController.setFXMLLoaderPrincipal(fxmlLoader);
+
+            if (!(idTecnico.getText().equals(""))) {
+
+                EstoqueController estoqueController = fxmlLoader.getController();
+                estoqueController.fazendoMudancaLogin(DAO.getTecnico().encontrarPorId(
+                        Integer.parseInt(idTecnico.getText())));
+            }
+
+            stage.show();
+        }
+        catch (java.io.IOException e) {
+
+        }
+
+    }
+
+    @FXML
+    void abaLogin (ActionEvent event) {
+
+        try {
+
+            if (MainController.getStageLogin() == null) {
+
+                FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("LoginView.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                Stage stage = new Stage();
+                stage.setResizable(false);
+                stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("/com/pbl/gerenciamentomicrocomputadores/Icones/Icone.png")));
+                stage.setScene(scene);
+
+                MainController.setStageLogin(stage);
+
+                stage.show();
+            }
+            else {
+
+                Stage stage = MainController.getStageLogin();
+                stage.show();
+                stage.toFront();
+            }
+        }
+        catch (java.io.IOException e) {
+
+        }
+
+    }
+
+    public void fazendoMudancaLogin (Tecnico tecnico) {
+
+        paneCantoInicio.setVisible(false);
+        paneTecnicoLogado.setVisible(true);
+
+        nomeTecnico.setText(tecnico.getNome());
+        idTecnico.setText(Integer.toString(tecnico.getId()));
+
+    }
+
+    @FXML
+    void abaCadastrar(ActionEvent event) {
+
+        try {
+
+            if (MainController.getStageCadastroTecnico() == null) {
+
+                FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("CadastrarTecnicoView.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                Stage stage = new Stage();
+                stage.setResizable(false);
+                stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("/com/pbl/gerenciamentomicrocomputadores/Icones/Icone.png")));
+                stage.setScene(scene);
+
+                MainController.setStageCadastroTecnico(stage);
+
+                stage.show();
+            }
+            else {
+
+                Stage stage = MainController.getStageCadastroTecnico();
+                stage.show();
+                stage.toFront();
+            }
+        }
+        catch (java.io.IOException e) {
+
+        }
+
+    }
+
+    @FXML
+    void deslogarAcao(ActionEvent event) {
+
+        paneCantoInicio.setVisible(true);
+        paneTecnicoLogado.setVisible(false);
+        nomeTecnico.setText("");
+        idTecnico.setText("");
+
+    }
+
+}
