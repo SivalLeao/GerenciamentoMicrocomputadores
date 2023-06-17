4f895efff8dbae6b62ecb77fc087f4f39bf11b21
Silvio <silviozv.oli@gmail.com>
2023-06-17 15:10:37 -0300

    feat: Colocando tipo de serviço no mini card de serviço do cliente.

--- src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/cards/MiniCardOrdemController.java
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/cards/MiniCardOrdemController.java
@@ -6,12 +6,26 @@ import javafx.scene.control.Label;
 
 public class MiniCardOrdemController {
 
-    @FXML
-    private Label idOrdem;
+    @FXML private Label idOrdem;
+    @FXML private Label tipoDeServico;
 
     public void setInfo (OrdemDeServico ordemDeServico) {
 
         idOrdem.setText(Integer.toString(ordemDeServico.getIdOrdem()));
+
+        if (ordemDeServico.getDescricaoServico().getTipoDeServico().equals("Montagem/Instalação")) {
+
+            tipoDeServico.setText(" Montagem/\n Instalação");
+        }
+        else if (ordemDeServico.getDescricaoServico().getTipoDeServico().equals("Sistema operacional")) {
+
+            tipoDeServico.setText(" Sistema\n Operacional");
+        }
+        else {
+
+            tipoDeServico.setText(" " + ordemDeServico.getDescricaoServico().getTipoDeServico());
+        }
+
     }
 
 }
