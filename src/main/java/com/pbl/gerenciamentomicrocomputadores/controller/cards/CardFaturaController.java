57452ade575c79af18942e2261cd324e9cde7283
Silvio <silviozv.oli@gmail.com>
2023-06-17 12:07:15 -0300

    feat: Card da fatura.

---
+++ src/main/java/com/pbl/gerenciamentomicrocomputadores/controller/cards/CardFaturaController.java
@@ -0,0 +1,33 @@
+package com.pbl.gerenciamentomicrocomputadores.controller.cards;
+
+import com.pbl.gerenciamentomicrocomputadores.controller.MyListener;
+import com.pbl.gerenciamentomicrocomputadores.model.OrdemDeServico;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class CardFaturaController {
+
+    @FXML
+    private Label idOrdem;
+
+    private OrdemDeServico ordemDeServico;
+
+    private MyListener<OrdemDeServico> myListener;
+
+    @FXML
+    public void click (MouseEvent mouseEvent) {
+
+        this.myListener.onClickListener(ordemDeServico);
+
+    }
+
+    public void setInfo (OrdemDeServico ordemDeServico, MyListener<OrdemDeServico> myListener) {
+
+        this.ordemDeServico = ordemDeServico;
+        this.myListener = myListener;
+
+        idOrdem.setText(Integer.toString(ordemDeServico.getIdOrdem()));
+    }
+
+}
